acf59087a2fe2a630e286ebee925807a475b0fca
Chance Hudson <jchancehud@gmail.com>
2019-06-14 10:22:17 -0500

    add webpack build system

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  target: 'node',
+  entry: {
+    main: './index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js',
+    publicPath: '/',
+  },
+}
